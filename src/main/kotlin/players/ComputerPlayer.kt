1c58dbf4906e6353fcb1918b5b2e4c9eba7df13f
Alex <you@example.com>
2020-10-15 00:28:41 +0100

    Add ComputerPlayer class

---
+++ src/main/kotlin/players/ComputerPlayer.kt
@@ -0,0 +1,9 @@
+package players
+
+import GameMove
+
+class ComputerPlayer: Player {
+    override fun getTurn(choiceOfMoves: List<GameMove>): GameMove {
+        return choiceOfMoves[(0 .. choiceOfMoves.size).random()]
+    }
+}
