2ed2f3b34c5629de48ccaaeed974d53fc21f52a9
Kerry Xu <kerry.xu18@imperial.ac.uk>
2020-10-15 21:50:43 +0100

    Implement human player

--- src/main/kotlin/Coordinate.kt
+++ src/main/kotlin/Coordinate.kt
@@ -1,1 +1,5 @@
-data class Coordinate(val x: Int, val y:Int)
+data class Coordinate(val x: Int, val y:Int) {
+    override fun toString(): String {
+        return ("($x, $y)")
+    }
+}
--- src/main/kotlin/players/HumanPlayer.kt
+++ src/main/kotlin/players/HumanPlayer.kt
@@ -1,10 +1,57 @@
 package players
 
+import Coordinate
 import GameMove
 
 class HumanPlayer: Player {
     override fun getTurn(choiceOfMoves: List<GameMove>): GameMove {
-        TODO("Not yet implemented")
-//        return choiceOfMoves[0]
+        fun isInteger(s: String?) = s?.toIntOrNull()?.let { true } ?: false
+
+        var possibleMoves: List<GameMove> = mutableListOf()
+        var input: String?
+
+        print("Enter the coordinate of the piece to move: ")
+
+        while (possibleMoves.isEmpty()) {
+            // number number e.g 0 1
+            input = readLine()
+            while (input == null || input.split(" ").size < 2) {
+                input = readLine()
+            }
+
+            val (x, y) = input.split(" ")
+
+            if (isInteger(x) || isInteger(y)) {
+                val pieceCoordinate = Coordinate(x.toInt(), y.toInt())
+
+                possibleMoves = choiceOfMoves.filter { m -> m.from == pieceCoordinate}
+
+                if (possibleMoves.isEmpty()) {
+                    print("No possible moves. Try again: ")
+                }
+            }
+        }
+
+        println("Select a move from the following: ")
+        for (i in possibleMoves.indices) {
+            println((i + 1).toString() + ": " + possibleMoves[i])
+        }
+
+        var index = -1
+        while (index <= 0) {
+            input = null
+            while (input == null) {
+                input = readLine()
+            }
+
+            if (isInteger(input)) {
+                index = input.toInt()
+                if (1 > index || possibleMoves.size < index) {
+                    index = -1
+                }
+            }
+        }
+
+        return possibleMoves[index - 1]
     }
 }
