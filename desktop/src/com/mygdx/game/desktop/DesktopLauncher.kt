59e40bdd769830b4b44b73b225f550f4203ff831
Alex <you@example.com>
2020-10-21 17:38:51 +0100

    Add core and desktop packages for libktx and change build.gradle

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.mygdx.game.MyGdxGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        config.height = 800
+        config.width = 800
+        LwjglApplication(MyGdxGame(), config)
+    }
+}
