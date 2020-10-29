eafae7b1e01bb8bd68e8001342c5c92a0bd42ca9
Sei Aoki <seiya.aoki18@imperial.ac.uk>
2020-10-29 17:36:14 +0000

    Implement Chess 960

---
+++ engine/src/main/kotlin/gameTypes/chess/Chess960.kt
@@ -0,0 +1,86 @@
+package main.kotlin.gameTypes.chess
+
+import main.kotlin.Coordinate
+import main.kotlin.boards.Board2D
+import main.kotlin.moves.visitors.Board2DMoveVisitor
+import main.kotlin.pieces.*
+
+class Chess960 : AbstractChess(){
+    override val board = Board2D(8, 8)
+    override val moveVisitor by lazy { Board2DMoveVisitor(board) }
+
+
+
+    override fun initGame() {
+        val player1 = players[0]
+        val player2 = players[1]
+        for (i in 0..7) {
+            board.addPiece(Coordinate(i, 1), WhitePawn(player1))
+            board.addPiece(Coordinate(i, 6), BlackPawn(player2))
+        }
+
+        /**
+         * White non pawns are placed randomly on the first rank following two rules
+         * 1. The bishop must be placed in opposite colour
+         * 2. The king must be placed between rooks
+         * **/
+
+        val pieces = listOf('R', 'B', 'N', 'Q', 'K', 'N', 'B', 'R')
+
+        val permutations = permute(pieces)
+
+        val regBishop = """.*B(..|....|......|)B.*""".toRegex()
+        val regKing = """.*R.*K.*R.*""".toRegex()
+        val possiblePermutations = mutableSetOf<String>()
+        for (permutation in permutations) {
+            val strPermutation = permutation.joinToString("")
+            if (strPermutation.matches(regBishop) && strPermutation.matches(regKing)) {
+                possiblePermutations.add(strPermutation)
+            }
+        }
+
+        val ree = possiblePermutations.random()
+
+        for ((i, c) in ree.withIndex()) {
+            when (c) {
+                'R' -> {
+                    board.addPiece(Coordinate(i, 0), Rook(player1))
+                    board.addPiece(Coordinate(i, 7), Rook(player2))
+                }
+                'B' -> {
+                    board.addPiece(Coordinate(i, 0), Bishop(player1))
+                    board.addPiece(Coordinate(i, 7), Bishop(player2))
+                }
+                'N' -> {
+                    board.addPiece(Coordinate(i, 0), Knight(player1))
+                    board.addPiece(Coordinate(i, 7), Knight(player2))
+                }
+                'Q' -> {
+                    board.addPiece(Coordinate(i, 0), Queen(player1))
+                    board.addPiece(Coordinate(i, 7), Queen(player2))
+                }
+                'K' -> {
+                    board.addPiece(Coordinate(i, 0), King(player1))
+                    board.addPiece(Coordinate(i, 7), King(player2))
+                }
+
+            }
+        }
+
+
+    }
+
+    private fun <T> permute(input: List<T>): List<List<T>> {
+        if (input.size == 1) return listOf(input)
+        val perms = mutableListOf<List<T>>()
+        val toInsert = input[0]
+        for (perm in permute(input.drop(1))) {
+            for (i in 0..perm.size) {
+                val newPerm = perm.toMutableList()
+                newPerm.add(i, toInsert)
+                perms.add(newPerm)
+            }
+        }
+        return perms
+    }
+}
