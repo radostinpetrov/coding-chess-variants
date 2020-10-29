fde0acb06d7c1fa4d74c12d95b6561d3fece07eb
Sei Aoki <seiya.aoki18@imperial.ac.uk>
2020-10-29 17:35:55 +0000

    Improve GameMove class to contain additional information for frontend access

--- engine/src/main/kotlin/GameMove.kt
+++ engine/src/main/kotlin/GameMove.kt
@@ -4,6 +4,28 @@ import main.kotlin.pieces.Piece
 import main.kotlin.players.Player
 
 sealed class GameMove(open val player: Player) {
-    data class BasicGameMove(val from: Coordinate, val to: Coordinate, val pieceMoved: Piece, override val player: Player, val pieceCaptured: Piece? = null, val piecePromotedTo: Piece? = null, val checkForCheck: Boolean = true) : GameMove(player)
-    data class CompositeGameMove(val gameMoves: List<BasicGameMove>, override val player: Player) : GameMove(player)
+    abstract val displayFrom: Coordinate
+    abstract var displayTo: Coordinate
+
+    data class BasicGameMove(val from: Coordinate, val to: Coordinate, val pieceMoved: Piece, override val player: Player, val pieceCaptured: Piece? = null, val piecePromotedTo: Piece? = null, val checkForCheck: Boolean = true) : GameMove(player) {
+        override val displayFrom: Coordinate
+            get() = from
+        override var displayTo: Coordinate = to
+    }
+
+    data class CompositeGameMove(val gameMoves: List<BasicGameMove>, override val player: Player) : GameMove(player) {
+        override val displayFrom: Coordinate
+            get() = gameMoves[0].from
+        override var displayTo: Coordinate = Coordinate(0, 0)
+
+        init {
+            val piece = gameMoves[0].pieceMoved
+            for (move in gameMoves.reversed()) {
+                if (move.pieceMoved == piece) {
+                    displayTo = move.to
+                    break
+                }
+            }
+        }
+    }
 }
