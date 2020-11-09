0b145dc780f863baadb5d464a761c452a4b59653
jimiin <pjm991110@gmail.com>
2020-11-09 20:23:21 +0000

    Implement palace logic

--- engine/src/main/kotlin/pieces/janggi/Cannon.kt
+++ engine/src/main/kotlin/pieces/janggi/Cannon.kt
@@ -5,9 +5,13 @@ import main.kotlin.pieces.Piece
 import main.kotlin.players.Player
 
 class Cannon(override val player: Player) : Piece {
+    private val palaceX = listOf(3, 5)
+    private val palaceY = listOf(0, 2, 7, 9)
+
     override val moveTypes: List<Move>
         get() = listOf(
-                Move.Hopper
+                Move.Hopper(HV = true),
+                Move.Restricted(Move.Hopper(D = true), palaceX, palaceY)
         )
 
     override fun getSymbol(): String {
--- engine/src/main/kotlin/pieces/janggi/RedSoldier.kt
+++ engine/src/main/kotlin/pieces/janggi/RedSoldier.kt
@@ -10,7 +10,11 @@ class RedSoldier(override val player: Player) : Piece {
         get() = listOf(
                 Move.Stepper(Direction.EAST, 1, true),
                 Move.Stepper(Direction.WEST, 1, true),
-                Move.Stepper(Direction.NORTH, 1, true)
+                Move.Stepper(Direction.NORTH, 1, true),
+                Move.Restricted(Move.Stepper(Direction.NORTH_EAST, 1, true), listOf(3), listOf(7)),
+                Move.Restricted(Move.Stepper(Direction.NORTH_EAST, 1, true), listOf(4), listOf(8)),
+                Move.Restricted(Move.Stepper(Direction.NORTH_WEST, 1, true), listOf(5), listOf(7)),
+                Move.Restricted(Move.Stepper(Direction.NORTH_WEST, 1, true), listOf(4), listOf(8)),
         )
 
     override fun getSymbol(): String {
