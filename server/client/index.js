21e50a27c33efac3783ad48ffd19328c5cc1e3e8
Kerry Xu <kerry.xu18@imperial.ac.uk>
2020-11-10 18:23:53 +0000

    Add basic server code

--- server/client/index.js
+++ server/client/index.js
@@ -1,11 +1,31 @@
-const WebSocket = require('ws')
-const ws = new WebSocket('ws://localhost:8080/');
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://localhost:8080');
 
 ws.on('open', function open() {
-  console.log("yet")
-  ws.send('matchmaking');
+  msg = {type: "matchmaking"}
+  ws.send(JSON.stringify(msg));
 });
 
 ws.on('message', function incoming(data) {
-  console.log(data);
+  obj = JSON.parse(data)
+  if (obj.type === "startGame") {
+    startGame(obj)
+  } else if (obj.type === "receiveMove") {
+    receiveMove(obj)
+  }
 });
+
+function startGame(obj) {
+  console.log("Starting game")
+    console.log(obj.player)
+    if (obj.player == 1) {
+      msg = {type: "makeMove", move: 0}
+    }
+}
+
+function receiveMove(obj) {
+  console.log("Received Move")
+  console.log(obj.move)
+  msg = {type: "makeMove", move: 0}
+}
--- server/index.js
+++ server/index.js
@@ -5,17 +5,34 @@ const uuid = require('uuid')
 const server = http.createServer();
 const wss = new WebSocket.Server({ server });
 
+const arr = []
+const matches = {}
+
 wss.on('connection', function connection(ws) {
   ws.id = uuid.v4();
   console.log(ws.id)
-  ws.on('matchmaking', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function incoming(message) {
+    obj = JSON.parse(message)
+    console.log(obj)
+    if (obj.type === "matchmaking"){
+      matchmaking(ws)
+    } else if (obj.type === 'makeMove') {
+      makemove(obj)
+    }
   });
-  ws.on('makeMove', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('startGame');
-  ws.send('receiveMove');
+  // ws.send('receiveMove');
 });
+
+function matchmaking(ws) {
+  arr.push(ws)
+    if (arr.length == 2) {
+      arr[0].send(JSON.stringify({type:"startGame", player:1}))
+      arr[1].send(JSON.stringify({type:"startGame", player:2}))
+    }
+}
+
+function makemove() {
+  
+}
 
 server.listen(8080);
