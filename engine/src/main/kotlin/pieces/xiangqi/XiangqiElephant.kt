deb2c7fb302b80c2a43d0e3f877b7090f6e75f8a
jimiin <pjm991110@gmail.com>
2020-11-11 16:16:48 +0000

    Implement horse and elephant move using composite move

--- engine/src/main/kotlin/pieces/xiangqi/XiangqiElephant.kt
+++ engine/src/main/kotlin/pieces/xiangqi/XiangqiElephant.kt
@@ -1,12 +1,18 @@
 package main.kotlin.pieces.xiangqi
 
+import main.kotlin.moves.Direction
 import main.kotlin.moves.Move
 import main.kotlin.pieces.Piece
 import main.kotlin.players.Player
 
 class XiangqiElephant(override val player: Player) : Piece {
     override val moveTypes: List<Move>
-        get() = listOf(Move.Leaper(2, 2))
+        get() = listOf(
+                Move.Composite(listOf(Move.Stepper(Direction.NORTH_EAST, 1, false), Move.Stepper(Direction.NORTH_EAST, 1, true))),
+                Move.Composite(listOf(Move.Stepper(Direction.NORTH_WEST, 1, false), Move.Stepper(Direction.NORTH_WEST, 1, true))),
+                Move.Composite(listOf(Move.Stepper(Direction.NORTH_EAST, 1, false), Move.Stepper(Direction.NORTH_EAST, 1, true))),
+                Move.Composite(listOf(Move.Stepper(Direction.SOUTH_EAST, 1, false), Move.Stepper(Direction.SOUTH_EAST, 1, true))),
+        )
 
     override fun getSymbol(): String {
         return "B"
