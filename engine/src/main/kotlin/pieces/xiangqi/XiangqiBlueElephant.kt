f1d7240f6fcb66f4e7317c0ea42af8be2515e3cc
jimiin <pjm991110@gmail.com>
2020-11-11 16:53:48 +0000

    Xianqi elephats can't cross the river

---
+++ engine/src/main/kotlin/pieces/xiangqi/XiangqiBlueElephant.kt
@@ -0,0 +1,22 @@
+package main.kotlin.pieces.xiangqi
+
+import main.kotlin.moves.Direction
+import main.kotlin.moves.Move
+import main.kotlin.pieces.Piece
+import main.kotlin.players.Player
+
+class XiangqiBlueElephant(override val player: Player) : Piece {
+    private val river = listOf(5, 6, 7, 8, 9)
+
+    override val moveTypes: List<Move>
+        get() = listOf(
+                Move.RestrictedDestination(Move.Stepper(Direction.NORTH_EAST, 2, true), listOf(), river),
+                Move.RestrictedDestination(Move.Stepper(Direction.NORTH_WEST, 2, true), listOf(), river),
+                Move.RestrictedDestination(Move.Stepper(Direction.SOUTH_EAST, 2, true), listOf(), river),
+                Move.RestrictedDestination(Move.Stepper(Direction.SOUTH_WEST, 2, true), listOf(), river),
+        )
+
+    override fun getSymbol(): String {
+        return "B"
+    }
+}
