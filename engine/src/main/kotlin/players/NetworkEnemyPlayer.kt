277a963a60246d392c3428db1ce019ea17d46153
Kerry Xu <kerry.xu18@imperial.ac.uk>
2020-11-12 15:56:18 +0000

    Change network enemy player to inherit form player

--- engine/src/main/kotlin/players/NetworkEnemyPlayer.kt
+++ engine/src/main/kotlin/players/NetworkEnemyPlayer.kt
@@ -2,12 +2,14 @@ package main.kotlin.players
 
 import main.kotlin.GameMove
 
-class NetworkEnemyPlayer : HumanPlayer() {
+class NetworkEnemyPlayer : Player {
     private var turnMoveIndex: Int? = null
 
     fun setGameMove(moveIndex: Int) {
         turnMoveIndex = moveIndex
     }
+
+    override var playerMove: GameMove? = null
 
     override fun getTurn(choiceOfMoves: List<GameMove>): GameMove? {
         if (turnMoveIndex == null) {
