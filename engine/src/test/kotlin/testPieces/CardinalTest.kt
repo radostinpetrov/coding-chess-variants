f56f30373d1c16169c6b4ed547d1df41cbc51719
Isabel  Li <isabel.li18@imperial.ac.uk>
2020-12-29 19:30:43 +0000

    Rename RandomTest to CardinalTest

--- engine/src/test/kotlin/testPieces/CardinalTest.kt
+++ engine/src/test/kotlin/testPieces/CardinalTest.kt
@@ -3,18 +3,13 @@ package testPieces
 import gameMoves.GameMove2D
 import gameTypes.chess.CapablancaChess
 import io.mockk.MockKAnnotations
-import io.mockk.mockk
 import io.mockk.spyk
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import pieces.chess.Cardinal
-import pieces.chess.King
-import pieces.chess.Knight
-import players.Player
 
-
-class RandomTest {
+class CardinalTest {
     val mockCapablancaChess = spyk<CapablancaChess>()
 
     @BeforeEach
