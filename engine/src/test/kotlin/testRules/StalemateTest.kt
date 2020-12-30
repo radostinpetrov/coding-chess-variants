647f44df4b85c7a88bf22d4c4e45968c1afeeb92
Kerry Xu <kerry.xu18@imperial.ac.uk>
2020-12-30 18:30:00 +0000

    Fix bug with stalemate test

--- engine/src/test/kotlin/testRules/StalemateTest.kt
+++ engine/src/test/kotlin/testRules/StalemateTest.kt
@@ -4,18 +4,16 @@ import coordinates.Coordinate2D
 import gameMoves.GameMove2D
 import gameTypes.chess.StandardChess
 import io.mockk.MockKAnnotations
-import io.mockk.mockk
 import io.mockk.spyk
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import pieces.chess.*
-import players.Player
 
 class StalemateTest {
     val mockStandardChess = spyk<StandardChess>()
     val board = mockStandardChess.board
-    
+
     val player1 = mockStandardChess.players[0]
     val player2 = mockStandardChess.players[1]
 
@@ -34,31 +32,41 @@ class StalemateTest {
             GameMove2D.BasicGameMove(Coordinate2D(7, 4), Coordinate2D(0, 4), Queen(player1), player1),
             GameMove2D.BasicGameMove(Coordinate2D(7, 6), Coordinate2D(7, 4), StandardBlackPawn(player2), player2),
             GameMove2D.BasicGameMove(
-                Coordinate2D(0, 4), Coordinate2D(2, 6), Queen(player1), player1, board.getPiece(
+                Coordinate2D(0, 4), Coordinate2D(2, 6), Queen(player1), player1,
+                board.getPiece(
                     Coordinate2D(2, 6)
-                )),
+                )
+            ),
             GameMove2D.BasicGameMove(
-                Coordinate2D(0, 5), Coordinate2D(7, 5), Rook(player2), player2, board.getPiece(
+                Coordinate2D(0, 5), Coordinate2D(7, 5), Rook(player2), player2,
+                board.getPiece(
                     Coordinate2D(7, 5)
-                )),
+                )
+            ),
             GameMove2D.BasicGameMove(Coordinate2D(7, 1), Coordinate2D(7, 3), StandardWhitePawn(player1), player1),
             GameMove2D.BasicGameMove(Coordinate2D(5, 6), Coordinate2D(5, 5), StandardBlackPawn(player2), player2),
             GameMove2D.BasicGameMove(Coordinate2D(2, 6), Coordinate2D(3, 6), Queen(player1), player1),
             GameMove2D.BasicGameMove(Coordinate2D(4, 7), Coordinate2D(5, 6), King(player2), player2),
             GameMove2D.BasicGameMove(
-                Coordinate2D(3, 6), Coordinate2D(1, 6), Queen(player1), player1, board.getPiece(
+                Coordinate2D(3, 6), Coordinate2D(1, 6), Queen(player1), player1,
+                board.getPiece(
                     Coordinate2D(1, 6)
-                )),
+                )
+            ),
             GameMove2D.BasicGameMove(Coordinate2D(3, 7), Coordinate2D(3, 2), Queen(player2), player2),
             GameMove2D.BasicGameMove(
-                Coordinate2D(1, 6), Coordinate2D(1, 7), Queen(player1), player1, board.getPiece(
+                Coordinate2D(1, 6), Coordinate2D(1, 7), Queen(player1), player1,
+                board.getPiece(
                     Coordinate2D(1, 7)
-                )),
+                )
+            ),
             GameMove2D.BasicGameMove(Coordinate2D(3, 2), Coordinate2D(7, 6), Queen(player2), player2),
             GameMove2D.BasicGameMove(
-                Coordinate2D(1, 7), Coordinate2D(2, 7), Queen(player1), player1, board.getPiece(
+                Coordinate2D(1, 7), Coordinate2D(2, 7), Queen(player1), player1,
+                board.getPiece(
                     Coordinate2D(2, 7)
-                )),
+                )
+            ),
             GameMove2D.BasicGameMove(Coordinate2D(5, 6), Coordinate2D(6, 5), King(player2), player2),
             GameMove2D.BasicGameMove(Coordinate2D(2, 7), Coordinate2D(4, 5), Queen(player1), player1),
         )
@@ -67,7 +75,7 @@ class StalemateTest {
             mockStandardChess.makeMove(move)
         }
 
-        mockStandardChess.getValidMoves(player2)
-        assertTrue(mockStandardChess.stalemate)
+        val outcome = mockStandardChess.getOutcome(player2)
+        assertEquals(Outcome.Draw("Stalemate by No Legal Moves"), outcome)
     }
 }
