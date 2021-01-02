be2ef64ecf0fe6f934ce51a1b86a6e2c1c711873
jimiin <pjm991110@gmail.com>
2021-01-02 19:07:44 +0000

    Change hashmap to list of moves

--- engine/src/main/kotlin/gameTypes/chess/winconditions/ThreeFoldRepetitionStalemate.kt
+++ engine/src/main/kotlin/gameTypes/chess/winconditions/ThreeFoldRepetitionStalemate.kt
@@ -18,21 +18,26 @@ import players.Player
  * The repeated positions need not occur in succession. ***/
 class ThreeFoldRepetitionStalemate : WinCondition<AbstractChess> {
     override fun evaluate(game: AbstractChess, player: Player, moves: List<GameMove2D>): Outcome? {
-        // Keep track of count of (player -> sorted list of valid moves) hashmap
-        val positionCounter: HashMap<HashMap<Player, List<GameMove2D>>, Int> = HashMap()
+        // Keep track of count of sorted list of valid moves
+        val positionCounter: HashMap<List<GameMove2D>, Int> = HashMap()
         val undoneMoves: MutableList<GameMove2D> = mutableListOf()
 
         for (i in game.moveLog.indices.reversed()) {
             val move = game.moveLog[i]
-
-            val currentPosition: HashMap<Player, List<GameMove2D>> = HashMap()
-            game.players.map { player ->
-                currentPosition[player] = game.getValidMoves(player)
-                    .sortedBy { it.hashCode() }
+            if (move.displayPieceCaptured != null) {
+                break
             }
+            println(move)
+
+//            val currentPosition: MutableList<GameMove2D> = mutableListOf()
+            val currentPosition: List<GameMove2D> = game.getValidMoves()
+//            game.players.map { currentPosition.addAll(game.getValidMoves(it)) }
+//            currentPosition.sortBy { it.hashCode() }
+            println(currentPosition)
 
             positionCounter[currentPosition] =
                 positionCounter.getOrDefault(currentPosition, 0) + 1
+            println(positionCounter[currentPosition])
             if (positionCounter[currentPosition] == 3) {
                 for (j in undoneMoves.indices.reversed()) {
                     game.makeMove(undoneMoves[j])
@@ -42,10 +47,6 @@ class ThreeFoldRepetitionStalemate : WinCondition<AbstractChess> {
 
             undoneMoves.add(move)
             game.undoMove()
-
-            if (move.displayPieceCaptured != null) {
-                break
-            }
         }
 
         for (i in undoneMoves.indices.reversed()) {
