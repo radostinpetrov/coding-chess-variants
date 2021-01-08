2ed04912364005873a87ef79f8d8b8811ba8e95c
Kerry Xu <kerry.xu18@imperial.ac.uk>
2021-01-08 17:45:03 +0000

    Add no repeated move from same piece rule

---
+++ engine/src/main/kotlin/tutorial/NoRepeatedMoveFromSamePieceRule.kt
@@ -0,0 +1,16 @@
+package tutorial
+
+import gameTypes.chess.AbstractChess
+import moves.Move2D
+import players.Player
+import rules.SpecialRules2D
+
+class NoRepeatedMoveFromSamePieceRule: SpecialRules2D<AbstractChess> {
+    override fun getPossibleMoves(game: AbstractChess, player: Player, moves: MutableList<Move2D>) {
+        if (game.moveLog.size < 2) {
+            return
+        }
+        val prevMove = game.moveLog[game.moveLog.size - 2]
+        moves.removeAll { it.displayPieceMoved == prevMove.displayPieceMoved }
+    }
+}
