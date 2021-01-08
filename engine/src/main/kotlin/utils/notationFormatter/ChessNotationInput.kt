4280eec91cb024180c28b5064a2850b04ed533ee
alex <axl18@ic.ac.uk>
2021-01-08 00:12:13 +0000

    Add comments to ChessNotationInput.kt.

--- engine/src/main/kotlin/utils/notationFormatter/ChessNotationInput.kt
+++ engine/src/main/kotlin/utils/notationFormatter/ChessNotationInput.kt
@@ -5,9 +5,17 @@ import moves.Move2D
 import moves.Move2D.CompositeMove
 import moves.Move2D.SimpleMove.*
 
+/**
+ * ChessNotationInput
+ * Converts standard ches notation to Coordinate2D and Coordinate2D,
+ * Move2D to readable string format. Is used for console front end.
+ */
 class ChessNotationInput() : NotationFormatter {
 
-    /* Converts the string representation of a coordinate to a coordinate. e.g A1 -> Coordinate(0, 6) */
+    /**
+     * Takes a String representation of a coordinate and converts to a Coordinate2D.
+     * @return Coordinate2D
+     */
     override fun strToCoordinate(s: String): Coordinate2D? {
         /* Check that the input string matches the pattern for standard chess notation. */
         val regex = """([a-z]+)(\d+)""".toRegex()
@@ -19,15 +27,22 @@ class ChessNotationInput() : NotationFormatter {
         return Coordinate2D(c1, c2)
     }
 
-    /* Converts a coordinate to the string representation of a coordinate. e.g Coordinate(0, 6) -> A1
-    * TODO: aa -> 27 etc.*/
+
+    //TODO: aa -> 27 etc.
+    /**
+     * Takes a Coordinate2D and converts it into standard chess notation. e.g Coordinate(0, 6) -> A1
+     * @returns string coordinate.
+     */
     override fun coordinateToStr(c: Coordinate2D): String {
         val c1 = (c.x + 'a'.toInt()).toChar()
         val c2 = c.y + 1
         return "$c1$c2"
     }
 
-    /* Get the string representation of a game move. */
+    /**
+     * Takes a Move2D and converts it to a readable chess notation.
+     * @return the string representing the move.
+     */
     override fun moveToStr(move: Move2D): String {
         val sb = StringBuilder()
 
@@ -56,6 +71,11 @@ class ChessNotationInput() : NotationFormatter {
         return sb.toString()
     }
 
+    /**
+     * Takes a BasicMove and converts it to a readable chess notation.
+     * Used as a helper function for moveToStr().
+     * @return the string representing the move.
+     */
     private fun basicMoveToStr(move: BasicMove): String {
         val sb = StringBuilder()
         sb.append("${move.pieceMoved.getSymbol()} moves from ${coordinateToStr(move.from)} to ${coordinateToStr(move.to)}")
