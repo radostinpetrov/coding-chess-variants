7ca719176725e069a6b0306260f2d55663f88515
alex <axl18@ic.ac.uk>
2021-01-08 20:48:13 +0000

    Add comments to ComputerPlayer.kt and FrontendPlayer.kt

--- core/src/players/FrontendPlayer.kt
+++ core/src/players/FrontendPlayer.kt
@@ -3,6 +3,10 @@ package players
 import com.badlogic.gdx.graphics.Color
 import screens.GameScreen
 
+/**
+ * Abstract class that is used to represent a player. Extend this class to make your own type of
+ * player. eg. Computer player, Human player (using the mouse as input commands).
+ */
 abstract class FrontendPlayer(val gameScreen: GameScreen, val colour: Color, val name: String) {
     val gameType = gameScreen.gameEngine
     // TODO any way to guarantee this to be initialized?
