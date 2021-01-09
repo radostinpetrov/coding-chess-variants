910a8e16bc008d6020c10cf91d466c72305b47dc
Kerry Xu <kerry.xu18@imperial.ac.uk>
2021-01-09 23:22:46 +0000

    Add tests for Chess960

--- engine/src/main/kotlin/gameTypes/chess/CapablancaChess.kt
+++ engine/src/main/kotlin/gameTypes/chess/CapablancaChess.kt
@@ -2,10 +2,10 @@ package gameTypes.chess
 
 import boards.Board2D
 import coordinates.Coordinate2D
-import rules.CapablancaCastling
-import rules.Enpassant
 import endconditions.StandardEndConditions
 import pieces.chess.*
+import rules.CapablancaCastling
+import rules.Enpassant
 
 /**
  * Represents Capablanca Chess
