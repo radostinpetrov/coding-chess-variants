6d14d9d484119241a38259431e0962c2b851fe6c
jimiin <pjm991110@gmail.com>
2021-01-10 16:20:25 +0000

    Null check for King and Refactor Abstract Castling

--- engine/src/main/kotlin/rules/AbstractCastling.kt
+++ engine/src/main/kotlin/rules/AbstractCastling.kt
@@ -1,5 +1,6 @@
 package rules
 
+import boards.Board2D
 import coordinates.Coordinate2D
 import gameTypes.chess.AbstractChess
 import moves.Move2D
@@ -40,11 +41,11 @@ abstract class AbstractCastling<C: AbstractChess> (
         val playerCanCastleLeft = if (player == game.players[0]) p1CanCastleLeft else p2CanCastleLeft
         val playerCanCastleRight = if (player == game.players[0]) p1CanCastleRight else p2CanCastleRight
         val rooks = checkRooks(game, player)
-        if (playerCanCastleLeft) {
-            castle(game, player, moves, rooks.first, CastlingDirection.LEFT)
+        if (playerCanCastleLeft && rooks.first != null) {
+            castle(game, player, moves, rooks.first!!, CastlingDirection.LEFT)
         }
-        if (playerCanCastleRight) {
-            castle(game, player, moves, rooks.second, CastlingDirection.RIGHT)
+        if (playerCanCastleRight && rooks.second != null) {
+            castle(game, player, moves, rooks.second!!, CastlingDirection.RIGHT)
         }
     }
 
@@ -52,15 +53,14 @@ abstract class AbstractCastling<C: AbstractChess> (
      * Helper functions
      */
 
-    private fun getKing(game: C, player: Player): Pair<Piece2D, Coordinate2D> {
+    private fun getKing(game: C, player: Player): Pair<Piece2D, Coordinate2D>? {
         return game.board.getPieces(player)
-            .find { p -> p.first.player == player && p.first is King }!!
+            .find { p -> p.first.player == player && p.first is King }
     }
 
-    private fun getRooks(game: C, player: Player): MutableList<Pair<Piece2D, Coordinate2D>> {
+    private fun getRooks(game: C, player: Player): List<Pair<Piece2D, Coordinate2D>> {
         return game.board.getPieces(player)
             .filter { p -> p.first.player == player && p.first is Rook }
-            .toMutableList()
     }
 
     /**
@@ -72,7 +72,7 @@ abstract class AbstractCastling<C: AbstractChess> (
     private fun canCastle(game: C, player: Player) : Boolean {
         val moveLog = game.moveLog
         val currentPlayerMoves = moveLog.filter { x -> x.player == player }
-        val rooks = getRooks(game, player)
+        val rooks = getRooks(game, player).toMutableList()
 
         for (move in currentPlayerMoves) {
             if (move.displayPieceMoved is King) {
@@ -92,113 +92,98 @@ abstract class AbstractCastling<C: AbstractChess> (
      */
     private fun checkRooks(game: C, player: Player): Pair<Coordinate2D?, Coordinate2D?> {
         val board = game.board
-
-        val king = getKing(game, player)
-        val kingPiece = king.first
-        val kingCoordinate = king.second
+        val king = getKing(game, player) ?: return Pair(null, null)
 
         val rooks = getRooks(game, player)
         var leftRook: Coordinate2D? = null
         var rightRook: Coordinate2D? = null
         for (rook in rooks) {
             val rookCoordinate = rook.second
-            if (rookCoordinate.x < kingCoordinate.x) {
+            if (rookCoordinate.x < king.second.x) {
                 leftRook = rookCoordinate
             } else {
                 rightRook = rookCoordinate
             }
         }
 
-        // Check Left Castling
-        if (leftRook != null) {
-            val rookPiece = board.getPiece(leftRook)
-            val lo = minOf(castleWidth - 1, kingCoordinate.x)
-            val hi = maxOf(castleWidth - 1, kingCoordinate.x)
-            for (x in lo until hi) {
-                val piece = board.getPiece(Coordinate2D(x, kingCoordinate.y))
-                if (piece != null && piece != rookPiece && piece != kingPiece) {
-                    leftRook = null
-                    break
-                }
-            }
-
-            val leftRookFinalDest = board.getPiece(Coordinate2D(castleWidth, kingCoordinate.y))
-            if (leftRookFinalDest != null && leftRook != null &&
-                leftRookFinalDest != kingPiece && leftRookFinalDest != rookPiece) {
-                leftRook = null
-            }
-        }
-
-        // Check Right Castling
-        if (rightRook != null) {
-            val rookPiece = board.getPiece(rightRook)
-            for (x in kingCoordinate.x + 1 until board.cols - 1) {
-                val piece = board.getPiece(Coordinate2D(x, kingCoordinate.y))
-                if (piece != null && piece != rookPiece && piece != kingPiece) {
-                    rightRook = null
-                    break
-                }
-            }
-
-            val rightRookFinalDest = board.getPiece(Coordinate2D(5, kingCoordinate.y))
-            if (rightRookFinalDest != null && rightRook != null &&
-                rightRookFinalDest != kingPiece && rightRookFinalDest != rookPiece) {
-                rightRook = null
-            }
-        }
-
+        leftRook = checkRook(board, king, leftRook, CastlingDirection.LEFT)
+        rightRook = checkRook(board, king, rightRook, CastlingDirection.RIGHT)
         return Pair(leftRook, rightRook)
+    }
+
+    private fun checkRook(board: Board2D, king: Pair<Piece2D, Coordinate2D>, rook: Coordinate2D?, dir: CastlingDirection): Coordinate2D? {
+        val kingPiece = king.first
+        val kingCoordinate = king.second
+
+        val range: IntRange
+        val finalX: Int
+        if (dir == CastlingDirection.LEFT) {
+            range = minOf(castleWidth - 1, kingCoordinate.x) until maxOf(castleWidth - 1, kingCoordinate.x)
+            finalX = castleWidth
+        } else {
+            range = kingCoordinate.x + 1 until board.cols - 1
+            finalX = board.cols - castleWidth
+        }
+
+        var rookCoordinate = rook
+        if (rookCoordinate != null) {
+            val rookPiece = board.getPiece(rookCoordinate)
+            for (x in range) {
+                val piece = board.getPiece(Coordinate2D(x, kingCoordinate.y))
+                if (piece != null && piece != rookPiece && piece != kingPiece) {
+                    rookCoordinate = null
+                    break
+                }
+            }
+            val rookFinalDest = board.getPiece(Coordinate2D(finalX, kingCoordinate.y))
+            if (rookFinalDest != null && rookCoordinate != null &&
+                rookFinalDest != kingPiece && rookFinalDest != rookPiece) {
+                rookCoordinate = null
+            }
+        }
+        return rookCoordinate
     }
 
     /**
      * Generates an appropriate moves to carry out castling and add to moves.
      * Assumes that it satisfies the pre-conditions for castling.
      */
-    private fun castle(game: C, player: Player, moves: MutableList<Move2D>, rookCoordinate: Coordinate2D?, dir: CastlingDirection) {
+    private fun castle(game: C, player: Player, moves: MutableList<Move2D>, rookCoordinate: Coordinate2D, dir: CastlingDirection) {
         val board = game.board
-        val king = getKing(game, player)
+        val king = getKing(game, player)?: return
         val kingPiece = king.first
         val kingCoordinate = king.second
 
-        if (rookCoordinate != null) {
-            val rook = board.getPiece(rookCoordinate)!!
-            val castlingMoves = mutableListOf<Move2D.SimpleMove>()
+        val rook = board.getPiece(rookCoordinate)!!
+        val castlingMoves = mutableListOf<Move2D.SimpleMove>()
+        castlingMoves.add(RemovePieceMove(player, rook, rookCoordinate))
 
-            castlingMoves.add(RemovePieceMove(player, rook, rookCoordinate))
-
-            val y = kingCoordinate.y
-            var currCoordinate = kingCoordinate
-            val finalKingX: Int
-            val finalRookX: Int
-            if (dir == CastlingDirection.LEFT) {
-                finalKingX = castleWidth - 1
-                finalRookX = castleWidth
-            } else {
-                finalKingX = board.cols - (castleWidth - 1)
-                finalRookX = board.cols - castleWidth
-            }
-
-            val range = if (kingCoordinate.x < finalKingX) {
-                kingCoordinate.x + 1 until finalKingX + 1
-            } else {
-                kingCoordinate.x - 1 downTo finalKingX
-            }
-
-            for (x in range) {
-                val nextCoordinate = Coordinate2D(x, y)
-                castlingMoves.add(
-                    BasicMove(currCoordinate, nextCoordinate, kingPiece, player)
-                )
-                currCoordinate = nextCoordinate
-            }
-
-            castlingMoves.add(AddPieceMove(player, rook, Coordinate2D(finalRookX, y)))
-
-            val move = CompositeMove(castlingMoves, player)
-            move.displayPieceMoved = kingPiece
-            move.displayFrom = kingCoordinate
-            move.displayTo = Coordinate2D(finalKingX, y)
-            moves.add(move)
+        val y = kingCoordinate.y
+        val finalKingX: Int
+        val finalRookX: Int
+        if (dir == CastlingDirection.LEFT) {
+            finalKingX = castleWidth - 1
+            finalRookX = castleWidth
+        } else {
+            finalKingX = board.cols - (castleWidth - 1)
+            finalRookX = board.cols - castleWidth
         }
+
+        val range = if (kingCoordinate.x < finalKingX)
+            kingCoordinate.x + 1 until finalKingX + 1 else kingCoordinate.x - 1 downTo finalKingX
+
+        var currCoordinate = kingCoordinate
+        for (x in range) {
+            val nextCoordinate = Coordinate2D(x, y)
+            castlingMoves.add(BasicMove(currCoordinate, nextCoordinate, kingPiece, player))
+            currCoordinate = nextCoordinate
+        }
+        castlingMoves.add(AddPieceMove(player, rook, Coordinate2D(finalRookX, y)))
+
+        val move = CompositeMove(castlingMoves, player)
+        move.displayPieceMoved = kingPiece
+        move.displayFrom = kingCoordinate
+        move.displayTo = Coordinate2D(finalKingX, y)
+        moves.add(move)
     }
 }
